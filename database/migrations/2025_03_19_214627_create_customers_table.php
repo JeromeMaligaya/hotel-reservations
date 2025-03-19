1e664444f41d2219bb1f0e3520284a53252f20cc
JeromeMaligaya <jeromehapi_97@yahoo.com>
2025-03-19 22:48:41 +0100

    added migration for customers table

---
+++ database/migrations/2025_03_19_214627_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable()->unique();
+            $table->string('passport_number')->nullable()->unique();
+            $table->string('id_card')->nullable()->unique();
+            $table->string('nationality')->nullable();
+            $table->text('special_requests')->nullable();
+            $table->boolean('customer_vip')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
